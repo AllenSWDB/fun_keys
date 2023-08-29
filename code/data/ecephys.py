c25d585be4732894cbe27704a9d0c0ce1b438d1b
adrianroggenbach <hoffmann@hifo.uzh.ch>
2023-08-29 23:22:49 +0000

    Add xarray functions

--- code/data/ecephys.py
+++ code/data/ecephys.py
@@ -107,7 +107,9 @@ def get_spike_matrix_10ms(units, session):
         # add spikes up in 10ms window
         spike_train[n,:] = np.add.reduceat(spikes_1kHz, np.arange(0,max_index,10))
 
-    data_xr = xr.DataArray(spike_train, dims=("unit_id", "time"), coords={"unit_id": unit_ids, "time": spike_time})
+    data_xr = xr.DataArray(spike_train.astype(np.int16),
+                           dims=("unit_id", "time"),
+                           coords={"unit_id": unit_ids, "time": spike_time})
     return data_xr
     
     
@@ -158,3 +160,118 @@ def easy_spike_matrix_and_unit_table(cache, session, with_layer=False):
                'dorsal_ventral_ccf_coordinate', 'left_right_ccf_coordinate', ]]
     return data_xr, units_reduced
     
+    
+    
+    
+def get_stim_xarray(trial_df, data_xr, image_int, active, start_dt=0, end_dt=0.5):
+    """ Return slices from the large data_xr at specific conditions and image numbers
+    
+    Returns xarray (nr_stims, nr_units, time) for given condition
+    
+    Usage:
+    from data import load_data, ecephys
+    trial_df = load_data.get_trial_df(session)
+    data_xr, units = ecephys.easy_spike_matrix_and_unit_table(cache, session)
+    image_int = 3    # can be between 0 and 7
+    active = True    # can be True or False
+    start_dt = -0.1  # will be added to 0, negative values for stim before
+    end_dt = 0.5     # end time (in seconds)
+    
+    stim_xr = get_stim_xarray(trial_df, data_xr, image_int, active, start_dt, end_dt)
+    """
+    
+    sel_trial = trial_df[ (trial_df.active==active) & (trial_df.image_int==image_int) ]
+    sel_data = list()
+    sel_stim_id = list()
+
+    for ind, start_t in sel_trial.start_time.items():
+        sel_stim_id.append(ind)
+        startInd = np.searchsorted(data_xr.time, start_t+start_dt)
+        endInd = np.searchsorted(data_xr.time, start_t+end_dt)  
+        sel_data.append( data_xr[:,startInd:endInd] )
+        
+    stim_arr = np.stack( [ar.data for ar in sel_data] )
+    time_part = sel_data[0].time.data - sel_data[0].time.data[0] + start_dt
+
+    stim_xr = xr.DataArray(stim_arr.astype(np.int16),
+                           dims=("stim_id", "unit_id", "time"),
+                           coords={"stim_id": sel_stim_id, 
+                                   "unit_id": data_xr.unit_id, "time": time_part}
+                          )
+    
+    # add meta-information
+    stim_xr.attrs['active'] = active
+    stim_xr.attrs['image_int'] = image_int
+    
+    period = 'Active' if active else 'Passive'
+    stim_xr.name = period + ', Image {}'.format(image_int)
+    
+    return stim_xr
+
+
+def get_all_stim_xarrays(trial_df, data_xr, start_dt=0, end_dt=0.5):
+    """ Returns a list with stim_xr for active/passive and all 8 images
+    
+    Element 0 in list: active, img 0
+    Element 1 in list: active, img 1
+    Element 8 in list: passive, img 0
+    
+    from data import load_data, ecephys
+    trial_df = load_data.get_trial_df(session)
+    data_xr, units = ecephys.easy_spike_matrix_and_unit_table(cache, session)
+    start_dt = 0     # will be added to 0, negative values for times before stim onset
+    end_dt = 0.5     # end time (in seconds)
+    
+    all_stim_xrs = get_all_stim_xarrays(trial_df, data_xr, start_dt, end_dt)
+    """
+    
+    all_stim_xrs = list()
+    for active in [True, False]:
+        for image_int in range(8):
+            stim_xr = get_stim_xarray(trial_df, data_xr, image_int,
+                                              active, start_dt=0, end_dt=0.5)
+            all_stim_xrs.append( stim_xr )
+        
+    return all_stim_xrs
+
+
+def get_presentation_xarray(presentation_table, data_xr, start_dt=0, end_dt=0.5,
+                            only_active_passive=True):
+    """ Return one data_xr with all presentations (all image presentations)
+    
+    Usage:
+    from data import load_data, ecephys
+    presentation_table = session.stimulus_presentations
+    data_xr, units = ecephys.easy_spike_matrix_and_unit_table(cache, session)
+    start_dt = 0      # will be added to 0, negative values for stim before
+    end_dt = 0.75     # end time (in seconds)
+    
+    pres_xr = get_presentation_xarray(presentation_table, data_xr, start_dt, end_dt)
+    """
+    
+    if only_active_passive:
+        pt = presentation_table
+        presentation_table = pt.loc[(pt.stimulus_block==0)|(pt.stimulus_block==5)]
+        
+    sel_trial = presentation_table
+    sel_data = list()
+    sel_stim_id = list()
+
+    for ind, start_t in sel_trial.start_time.items():
+        sel_stim_id.append(ind)
+        startInd = np.searchsorted(data_xr.time, start_t+start_dt)
+        endInd = np.searchsorted(data_xr.time, start_t+end_dt)  
+        sel_data.append( data_xr[:,startInd:endInd] )
+        
+    stim_arr = np.stack( [ar.data for ar in sel_data] )
+    time_part = sel_data[0].time.data - sel_data[0].time.data[0] + start_dt
+
+    pres_xr = xr.DataArray(stim_arr.astype(np.int16), dims=("stim_id", "unit_id", "time"),
+                           coords={"stim_id": sel_stim_id, 
+                                   "unit_id": data_xr.unit_id, "time": time_part}
+                          )
+    
+    # add meta-information
+    pres_xr.name = 'All stim presentations'
+    
+    return pres_xr
